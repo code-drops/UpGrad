ad4cc809e919ff2b91aedf8a450be512e2cdb4d4
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-06-24 11:44:29 +0530

    Add files via upload

---
+++ python for data science/2. Data cleaning/bank_data_cleaning.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+
+# to remove the warnings
+warnings.filterwarnings("ignore")
+
+# read csv file and skipping first two rows
+bank = pd.read_csv("bank_marketing_updated_v1.csv",skiprows=2,index_col="customerid")
+
+################## Fixing the rows and columns #################################
+# # correcting the jobedu column
+bank["job"] = bank.jobedu.apply(lambda x:x.split(',')[0])
+bank["education"] = bank.jobedu.apply(lambda x:x.split(',')[1])
+del bank["jobedu"]
+
+#number of missing values in all columns
+print(bank.isnull().sum())
+
+# there are 50 records which have NaN values in them in month column
+print(len(bank[bank.month.apply(lambda x: True if type(x)==float else False)==True]))
+
+
+############ handling the missing values #################################
+
+# dropping the records with missing age
+bank = bank[-bank.age.isnull()]
+
+# # # handling missing month
+# value and their count
+print(bank['month'].value_counts())
+
+mode = bank['month'].mode()[0]
+
+# filling the NA/NaN values
+bank['month'] = bank['month'].fillna(mode)
+
+# handling -1 in pdays
+# replacing the -1 with NaN value in order to not take them in mean , mode or median operation
+bank['pdays'] = bank['pdays'].apply(lambda x: np.NaN if x<0 else x)
+print(bank['pdays'].describe())
+
+##### Handling outliers
+
+# watching outliers in age columns
+bank['age'].plot(kind="box")              #valid outliers
+plt.show()
+
+# outliers in balance column
+bank['balance'].plot(kind="box")           # valid outliers
+plt.show()
+
+####################### standardizing dataset
+# some values are in min and some are in seconds
+bank['duration'] = bank['duration'].apply(lambda x : float(x.split(' ')[0])/60 if x.endswith("sec") else x.split(' ')[0])
